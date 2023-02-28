6f117455cb52c6d2e0526d5f9d0d1fa0c43b89c0
antoniloandrade4@gmail.com <110502881+Antonilo-de-Andrade@users.noreply.github.com>
2023-02-28 11:58:32 -0300

    add_section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,46 +1,89 @@
-// We import the CSS which is extracted to its own file by esbuild.
-// Remove this line if you add a your own CSS build pipeline (e.g postcss).
-import "../css/app.css"
 
-// If you want to use Phoenix channels, run `mix help phx.gen.channel`
-// to get started and then uncomment the line below.
-// import "./user_socket.js"
+<head>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+            }
+		gtag('js', new Date());
 
-// You can include dependencies in two ways.
-//
-// The simplest option is to put them in assets/vendor and
-// import them using relative paths:
-//
-//     import "../vendor/some-package.js"
-//
-// Alternatively, you can `npm install some-package --prefix assets` and import
-// them using a path starting with the package name:
-//
-//     import "some-package"
-//
-
-// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
-// Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
-import topbar from "../vendor/topbar"
-
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken},
-hooks})
-
-// Show progress bar on live navigation and form submits
-topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
-window.addEventListener("phx:page-loading-start", info => topbar.show())
-window.addEventListener("phx:page-loading-stop", info => topbar.hide())
-
-// connect if there are any LiveViews on the page
-liveSocket.connect()
-
-// expose liveSocket on window for web console debug logs and latency simulation:
-// >> liveSocket.enableDebug()
-// >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
-// >> liveSocket.disableLatencySim()
-window.liveSocket = liveSocket
-
+		gtag('config', 'G-JK0NXFS7RB');
+	</script>
+	<!-- Google Tag Manager -->
+	<script>
+		(function(w, d, s, l, i) {
+			w[l] = w[l] || [];
+		w[l].push({
+			'gtm.start': new Date().getTime(),
+		event: 'gtm.js'
+                });
+		var f = d.getElementsByTagName(s)[0]
+		, j = d.createElement(s)
+		, dl = l != 'dataLayer' ? '&l=' + l : '';
+		j.async = true;
+		j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+		f.parentNode.insertBefore(j, f);
+            }
+		)(window, document, 'script', 'dataLayer', 'GTM-WCG3PF2');
+	</script>
+	<!-- End Google Tag Manager -->
+	<!-- Matomo -->
+	<script>
+		var _paq = window._paq = window._paq || [];
+		/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+		_paq.push(['trackPageView']);
+		_paq.push(['enableLinkTracking']);
+		(function() {
+                var u = "https://stats.h2sistemas.com.br/";
+		_paq.push(['setTrackerUrl', u + 'matomo.php']);
+		_paq.push(['setSiteId', '9']);
+		var d = document
+		, g = d.createElement('script')
+		, s = d.getElementsByTagName('script')[0];
+		g.async = true;
+		g.src = u + 'matomo.js';
+		s.parentNode.insertBefore(g, s);
+            }
+		)();
+	</script>
+	<!-- End Matomo Code -->
+	<!-- Facebook Pixel Code -->
+	<script>
+		!function(f, b, e, v, n, t, s) {
+                if (f.fbq)
+		return;
+		n = f.fbq = function() {
+			n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+		}
+		;
+		if (!f._fbq)
+		f._fbq = n;
+		n.push = n;
+		n.loaded = !0;
+		n.version = '2.0';
+		n.queue = [];
+		t = b.createElement(e);
+		t.async = !0;
+		t.src = v;
+		s = b.getElementsByTagName(e)[0];
+		s.parentNode.insertBefore(t, s)
+            }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+		fbq('init', '225297752866639');
+		fbq('track', 'PageView');
+	</script>
+	<noscript>
+		<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=225297752866639&ev=PageView&noscript=1" />
+	</noscript>
+	<!-- End Facebook Pixel Code -->
+	<meta name="facebook-domain-verification" content="nxks4o8z2j88h3qv3o6avgbzuhg5u4" />
+	<script>
+		window.AUTHED = "false";
+		window.TOKEN = "";
+		window.INTERNAL = "hafeng";
+		window.GRUUID = "";
+		window.GR3CLIENT = "";
+		window.GR2CLIENT = "";
+	</script>
+	<script src="/js/phoenix.min.js"></script>
+	<script src="/js/phoenix_live_view.js"></script>
+</head>
